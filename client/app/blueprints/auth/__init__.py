6e1741060ef877330a89b952e320b1d258a302de
Marcos Reyes <astronautmarkus@gmail.com>
2026-08-20 00:15:12 -0400

    refactor: renamed project to PAYDAY BANK.

--- client/app/blueprints/auth/__init__.py
+++ client/app/blueprints/auth/__init__.py
@@ -15,7 +15,7 @@ def login_required(view):
     @wraps(view)
     def wrapped_view(*args, **kwargs):
         if "user_id" not in session:
-            flash("Inicia sesión para entrar al banco.", "error")
+            flash("Inicia sesión para entrar a PAYDAY BANK.", "error")
             return redirect(url_for("auth.login"))
         return view(*args, **kwargs)
 
